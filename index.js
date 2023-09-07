7dc007a13be792e1c06289766c0fcde210546ce8
jfmendesDeveloper <141954097+jfmendesDeveloper@users.noreply.github.com>
2023-09-07 22:22:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Cloud Infrastructure and Virtualization performed by Craig and Joao. come on'); //write a response to the client
   res.end(); //end the response
-}).listen(4000); //the server object listens on port 4000
+}).listen(3000); //the server object listens on port 3000
