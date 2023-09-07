79cf0a8e8f8147379caa43bad9df8b20f7e4caea
jfmendesDeveloper <141954097+jfmendesDeveloper@users.noreply.github.com>
2023-09-07 21:29:32 +0100

    Testing

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Cloud Infrastructure and Virtualization performed by Craig and Joao. Testing phase'); //write a response to the client
+  res.write('Cloud Infrastructure and Virtualization performed by Craig and Joao. Testing phase 1'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
