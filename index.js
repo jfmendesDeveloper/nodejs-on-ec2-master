917b3be0f0c41119567f6886a2426c1adbc67089
jfmendesDeveloper <141954097+jfmendesDeveloper@users.noreply.github.com>
2023-09-07 21:21:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,5 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
   res.write('Cloud Infrastructure and Virtualization oerformed by Craig and Joao.testing'); //write a response to the client
-  p Welcome to the Application:
-  p 1. Create EC2 Instance (Ubuntu image)
-  p 2. Install and configure Jenkins on Ubuntu
-  p 3. Install node and npm
-  p 4. Install docker on ubuntu 
-  p 5. Create a docker file
-  p 6. Github as source code repo
-  p 7. Create CI CD pipeline
-  res.end(); //end the response
+   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
