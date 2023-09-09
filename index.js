369cf2b38dbb046f5e239433a873291e4da686c7
jfmendesDeveloper <141954097+jfmendesDeveloper@users.noreply.github.com>
2023-09-09 11:28:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Cloud Infrastructure and Virtualization. This is the final presentation. Assignment performed by Craig and Joao'); //write a response to the client
+  res.write('Cloud Infrastructure and Virtualization. Assignment performed by Craig and Joao. This is the CA for DBS'); //write a response to the client
   res.end(); //end the response
-}).listen(5000); //the server object listens on port 5000
+}).listen(4000); //the server object listens on port 4000
